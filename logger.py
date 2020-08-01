b9beee6f847a0c6bf61f46125328fad2a35dbdd1
TangliziGit <tanglizimail@foxmail.com>
2020-08-01 11:36:14 +0800

    update logger, add QTable params

--- logger.py
+++ logger.py
@@ -6,6 +6,7 @@ from config import Config
 
 
 class Logger:
+    _q_tables = None
     _filename = f"log/{time.strftime('%Y-%m-%d_%H:%M:%S', time.localtime(time.time()))}.log"
     _file = None
     if not Config.TEST:
@@ -14,6 +15,10 @@ class Logger:
     _prefix = '-->'
     _iteration = '-' * 25
     _error = '!' * 30
+
+    @staticmethod
+    def set_q_tables(q_tables):
+        Logger._q_tables = q_tables
 
     @staticmethod
     def iteration_start(it, content = None):
@@ -43,20 +48,43 @@ class Logger:
         Logger.print(e)
 
     @staticmethod
-    def show_params():
-        table = PrettyTable(["Type", "ofFilter", "FilterHeight", "FilterWidth", "StrideHeight", "StrideWidth"])
+    def show_status(is_stable=False):
+        if is_stable:
+            Logger.print(f"{'#' * 30} STABLE STATUS {'#' * 30}")
+
+        # show param status
+        table = PrettyTable(["Type", "OfFilter", "FilterHeight", "FilterWidth", "StrideHeight", "StrideWidth", "Acc"])
 
         table.add_row(['current', Config.of_filter,
                        Config.filter_height, Config.filter_width,
-                       Config.stride_height, Config.stride_width])
+                       Config.stride_height, Config.stride_width,
+                       f"{Config.last_accuracy} (last)"])
 
         table.add_row(['best', Config.best_of_filter,
                        Config.best_filter_height, Config.best_filter_width,
-                       Config.best_stride_height, Config.best_stride_width])
+                       Config.best_stride_height, Config.best_stride_width,
+                       Config.best_acc])
 
         table.add_row(['worst', Config.worst_of_filter,
                        Config.worst_filter_height, Config.worst_filter_width,
-                       Config.worst_stride_height, Config.worst_stride_width])
+                       Config.worst_stride_height, Config.worst_stride_width,
+                       Config.worst_acc])
 
         Logger.print(str(table))
         Logger.print("")
+
+        # show q table status
+        def get_q_table_str(name, df):
+            Logger.stage("QTable", f"{name} (0 for Sub, 1 for Add)")
+            qtable = PrettyTable(['Status'] + list(df.columns.array))
+            for row_name, row in zip(df.index.values, df.values):
+                qtable.add_row([row_name] + list(row))
+            return str(qtable)
+
+        Logger.print(get_q_table_str("OfFilter", Logger._q_tables.RL_table_of_filter.q_table))
+        Logger.print(get_q_table_str("FilterHeight", Logger._q_tables.RL_table_filter_height.q_table))
+        Logger.print(get_q_table_str("FilterWidth", Logger._q_tables.RL_table_filter_width.q_table))
+        Logger.print(get_q_table_str("StrideHeight", Logger._q_tables.RL_table_stride_height.q_table))
+        Logger.print(get_q_table_str("StrideWidth", Logger._q_tables.RL_table_stride_width.q_table))
+        Logger.print("")
+
